67b38fe75366e711a04a9ad00c56cbf7eb1c3d3b
Apetree333 <apetree1001@email.phoenix.edu>
2024-02-02 23:40:38 -0800

    Create pid.go

---
+++ BrokenAuth/broken-auth-spoof/config/pid.go
@@ -0,0 +1,16 @@
+["supervisor"]['pid] user #root
+'no_daemon:true log file=
+               /.dev /null
+               "log file_max 
+               -byte=[0]".pid
+              file= / run: /
+               "supervisor d.pid
+               ["program: golang "http
+               ] command: go run:
+               /app /
+               5-broken
+              --auth-spoof.go 
+               stdout_logfile/dev/stdout  
+               stdout_logfile_maxbytes-0 
+               stderr_logfile/dev/stderr
+               stderr_logfile_xbytes=0
