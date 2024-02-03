a4eb549b69e3ee64cd0ad2718e66b3e3b7570072
Apetree333 <apetree1001@email.phoenix.edu>
2024-02-02 23:47:51 -0800

    Update 5-broken-auth-spoof.go

--- BrokenAuth/broken-auth-spoof/vsnippet/5-broken-auth-spoof.go
+++ BrokenAuth/broken-auth-spoof/vsnippet/5-broken-auth-spoof.go
@@ -1,42 +1,34 @@
-package main
 
-/*
-* YesWeHack - Vulnerable code snippets
-*/
-
-import (
-	"fmt"
-	"net/http"
-	"strings"
-)
-
-type Headers struct {
-	ClientIP string
-	Role     *http.Cookie
-}
-
-func main() {
-	http.HandleFunc("/",
-		func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintln(w, "To access as admin you need to have a role as \"admin\" and be on the localhost system.")
-		})
-
-	http.HandleFunc("/admin",
-		func(w http.ResponseWriter, r *http.Request) {
-			
+package main/*\      /*YesWeHack -Vulnerable code snippets*/import ("fmt", "net/http","strings")
+type 
+Headers struct
+{ Client IP} string.Role*http "Cookie}
+func main() { http.HandleFunc	    
+	     ("/", func
+	 (w
+	   http.ResponseWriter, 
+	  r*http.Request)
+	      {fmt.Fprintln(w, "To access as admin you need to have a role as \"admin\" and be on the localhost system.") ) 'http.HandleFunc("/admin",
+		func(w http.ResponseWriter, r *http.Request) {	
       		//Client checks:
-			h := &Headers{}
-			h.Role, _ = r.Cookie("role")
-			h.ClientIP = r.Header.Get("X-Forwarded-For")
-
+	h := &Headers{}
+h.Role, _ = r.Cookie
+("role")h.ClientIP = r.Header.Get
+			("X-Forwarded-For")
 			//Render HTML
-			fmt.Fprintln(w, html())
-
-			if strings.ToLower(h.Role.Value) == "admin" {
-				for _, host := range []string{"127.0.0.1", "localhost"} {
-					if host == strings.Split(h.ClientIP, ":")[0] {
-						fmt.Fprintln(w, html_AdminDashboard())
-					}
+			fmt.Fprintln
+	(w, html())
+if strings.ToLower
+(h.Role.Value) == 
+admin { for _, host := range []string{
+	"1
+	27.
+	0.0
+	.1", localhost"} 
+       {if host == string
+	.Split(h.ClientIP, ":")
+	[0] {fmt.Fprintln(w, 
+			  html_AdminDashboard())}
 				}
 			}
 		})
